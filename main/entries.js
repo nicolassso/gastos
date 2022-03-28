ce3377100ee80558dd5f02aeb8354bdf5bfad62e
Nicolás Leal <49799875+nicolassso@users.noreply.github.com>
2022-03-29 00:33:12 +0200

    added prettier (not working tho)

--- main/entries.js
+++ main/entries.js
@@ -4,5 +4,4 @@
 import * as fs from 'fs'
 import { isEmptyData } from '../utils.js';
 
-export const MARCH_ENTRIES = isEmptyData(fs.readFileSync(`../data/months/march/entries.json`, 'utf8'))[0].MARCH_ENTRIES
-
+export const MARCH_ENTRIES = isEmptyData(fs.readFileSync('../data/months/march/entries.json', 'utf8'))[0].MARCH_ENTRIES
