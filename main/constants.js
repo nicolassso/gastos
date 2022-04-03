03bd2ac52a3ddbaabea498e0b631f37a7d1216d6
Nicolás Leal <49799875+nicolassso@users.noreply.github.com>
2022-04-03 20:46:45 +0200

    Create currentMonth constant and add April entries

---
+++ main/constants.js
@@ -0,0 +1,17 @@
+const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
+const d = new Date();
+
+export const currentMonth = monthNames[d.getMonth()];
--- main/entries.js
+++ main/entries.js
@@ -3,7 +3,11 @@
  */
 import * as fs from 'fs';
 import { storedData } from '../utils.js';
+import { currentMonth } from './constants.js';
 
 export const MARCH_ENTRIES = storedData(
     fs.readFileSync('../data/months/march/entries.json', 'utf8')
 )[0].MARCH_ENTRIES;
+export const APRIL_ENTRIES = storedData(
+    fs.readFileSync('../data/months/april/entries.json', 'utf8')
+)[0].APRIL_ENTRIES;
